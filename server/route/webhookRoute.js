b83b63bf2039fa2b2436ee2f2d873867712ea42d
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-20 16:02:39 +0530

    remove: delete Stripe webhook route

--- server/route/webhookRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { stripeWebhook } from "../controllers/webhookController.js";
-
-const router = express.Router();
-
-// Note: Stripe requires the raw body to construct the event
-router.post("/stripe", express.raw({ type: "application/json" }), stripeWebhook);
-
-export default router;
